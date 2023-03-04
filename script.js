dd48435abeaf050161ffb4fef971800197990fec
SabanadzeLuka <123399167+SabanadzeLuka@users.noreply.github.com>
2023-03-04 02:52:55 -0800

    ///

---
+++ script.js
@@ -0,0 +1,30 @@
+const cifrebiEl = document.getElementById("cifrebi");
+const buttonEL = document.getElementById("btn");
+
+const numbers = "0 1 2 3 4 5 6 7 8 9 10"
+
+let index = 1
+let speed = 200
+
+buttonEL.addEventListener("click", function() {
+  function writeNumbers() {
+    cifrebiEl.innerText = numbers.slice(0, index)
+    if (index < numbers.length) {
+        index++
+        setTimeout(writeNumbers,speed)
+    }
+    else {
+        setTimeout(decreaseNumbers, speed)
+    }
+  }
+
+  function decreaseNumbers() {
+    cifrebiEl.innerText = numbers.slice(0, index)
+    if (index > 0) {
+        index--
+        setTimeout(decreaseNumbers, speed)
+    }
+  }
+
+  writeNumbers();
+});
