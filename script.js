364b0deeaee55e8290cd5eedc76347d9e4486e4f
SabanadzeLuka <123399167+SabanadzeLuka@users.noreply.github.com>
2023-03-04 06:19:43 -0800

    123

--- script.js
+++ script.js
@@ -1,28 +1,27 @@
 const cifrebiEl = document.getElementById("cifrebi");
 const buttonEL = document.getElementById("btn");
 
-const numbers = "0 1 2 3 4 5 6 7 8 9 10"
+const numbers = "0 1 2 3 4 5 6 7 8 9 10";
 
-let index = 1
-let speed = 200
+let index = 1;
+let speed = 200;
 
-buttonEL.addEventListener("click", function() {
+buttonEL.addEventListener("click", function () {
   function writeNumbers() {
-    cifrebiEl.innerText = numbers.slice(0, index)
+    cifrebiEl.innerText = numbers.slice(0, index);
     if (index < numbers.length) {
-        index++
-        setTimeout(writeNumbers,speed)
-    }
-    else {
-        setTimeout(decreaseNumbers, speed)
+      index++;
+      setTimeout(writeNumbers, speed);
+    } else {
+      setTimeout(decreaseNumbers, speed);
     }
   }
 
   function decreaseNumbers() {
-    cifrebiEl.innerText = numbers.slice(0, index)
+    cifrebiEl.innerText = numbers.slice(0, index);
     if (index > 0) {
-        index--
-        setTimeout(decreaseNumbers, speed)
+      index--;
+      setTimeout(decreaseNumbers, speed);
     }
   }
 
