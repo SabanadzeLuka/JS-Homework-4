4d31f02f3e7d9ce3dcc495d06b71c131082f3b1f
SabanadzeLuka <123399167+SabanadzeLuka@users.noreply.github.com>
2023-03-05 03:05:44 -0800

    </>

--- script.js
+++ script.js
@@ -1,29 +1,32 @@
-const cifrebiEl = document.getElementById("cifrebi");
-const buttonEL = document.getElementById("btn");
+const numbersEl = document.getElementById("cifrebi");
+const buttonEl = document.getElementById("btn");
+let speed = 400;
 
-const numbers = "0 1 2 3 4 5 6 7 8 9 10";
+const numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+let index = 0;
 
-let index = 1;
-let speed = 200;
-
-buttonEL.addEventListener("click", function () {
-  function writeNumbers() {
-    cifrebiEl.innerText = numbers.slice(0, index);
-    if (index < numbers.length) {
-      index++;
-      setTimeout(writeNumbers, speed);
-    } else {
-      setTimeout(decreaseNumbers, speed);
-    }
+function writeNumbers() {
+  if (index < numbers.length) {
+    numbersEl.innerText = numbers[index];
+    index++;
+    setTimeout(writeNumbers, speed);
+  } else {
+    setTimeout(decreaseNumbers, speed);
   }
+}
 
-  function decreaseNumbers() {
-    cifrebiEl.innerText = numbers.slice(0, index);
-    if (index > 0) {
-      index--;
-      setTimeout(decreaseNumbers, speed);
-    }
+function decreaseNumbers() {
+  if (index > 0) {
+    index--;
+    numbersEl.innerText = numbers[index];
+    setTimeout(decreaseNumbers, speed);
+  } else {
+    numbersEl.innerText = "Click to load";
+    buttonEl.removeAttribute("disabled");
   }
+}
 
+buttonEl.addEventListener("click", function() {
   writeNumbers();
+  buttonEl.setAttribute("disabled", true);
 });
